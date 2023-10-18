87186e0276844a09801ed711a25d28fefb8b05a0
SeleznevDaniil <148335350+SeleznevDaniil@users.noreply.github.com>
2023-10-18 22:23:25 +0300

    Update task_1.1.py

--- лабораторная 1/task_1.1.py
+++ лабораторная 1/task_1.1.py
@@ -1,3 +1,15 @@
-lst = list(map(float, input().split()))
-lst.append(sum(lst) / (len(lst) + 1))
-print(lst)
+numbers = list(map(float, input().split()))
+sum = sum(numbers)
+length = len(numbers) + 1
+
+current = 0
+currentWasSet = True
+lostIndex = -1
+for number in numbers:
+    lostIndex += 1
+    if(number - current > 1.0 and currentWasSet):
+        break
+    currentWasSet = True
+    current = number
+numbers.insert(lostIndex, sum / length)
+print(numbers)
